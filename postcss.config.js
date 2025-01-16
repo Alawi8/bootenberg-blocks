691f8539c0617f7a4fdf8ddcd531826d2ff297b7
Alawi8 <alialshalawi9@gmail.com>
2025-01-16 14:03:08 +0300

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const classRename = require('postcss-class-rename');
 module.exports = {
     plugins: [
         classRename({
-            '\\b(\\w+)-': 'tw-$1-', // إضافة بادئة "tw-" لجميع الكلاسات
+            '\\b(\\w+)-': 'tw-$1-', 
         }),
         require('tailwindcss'),
         require('autoprefixer'),
